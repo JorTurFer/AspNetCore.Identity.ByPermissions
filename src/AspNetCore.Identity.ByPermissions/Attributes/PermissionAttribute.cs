d62ac1b35a8f5952799b250827829c759bb55d1a
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-08-08 22:11:55 +0200

    Documentation of permission attribute

--- src/AspNetCore.Identity.ByPermissions/Attributes/PermissionAttribute.cs
+++ src/AspNetCore.Identity.ByPermissions/Attributes/PermissionAttribute.cs
@@ -5,13 +5,23 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace AspNetCore.Identity.ByPermissions
 {
-  public class PermissionAttribute : AuthorizeAttribute
-  {
-    public PermissionAttribute(string Permission,string Description):base(Permission)
+    /// <summary>
+    /// Attribute for trigger the auto Permission detect
+    /// </summary>
+    public class PermissionAttribute : AuthorizeAttribute
     {
-      this.Description = Description; 
+        /// <summary>
+        /// Creates a permission
+        /// </summary>
+        /// <param name="Permission">Permision name</param>
+        /// <param name="Description">Description to show</param>
+        public PermissionAttribute(string Permission, string Description) : base(Permission)
+        {
+            this.Description = Description;
+        }
+        /// <summary>
+        /// Description of the permission
+        /// </summary>
+        public string Description { get; }
     }
-
-    public string Description { get;}
-  }
 }
