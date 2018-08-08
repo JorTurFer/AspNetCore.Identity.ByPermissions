b5c3aa8cfcf179358e5c2fb8556d517227b38697
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-08-08 22:51:31 +0200

    Add doc to GetPermissionsManageViewModel

--- src/Identity By Permissions Example/Helpers/PermissionsManageHelper.cs
+++ src/Identity By Permissions Example/Helpers/PermissionsManageHelper.cs
@@ -10,12 +10,19 @@ namespace Identity_By_Permissions_Example.Helpers
 {
     public class PermissionsManageHelper
     {
+        /// <summary>
+        /// Generate the Permission manage ViewModel
+        /// </summary>
+        /// <param name="claims">User Claims</param>
+        /// <param name="role">Role to check</param>
+        /// <param name="permissionService">Permissions Service</param>
+        /// <returns></returns>
         public static PermissionsManageViewModel GetPermissionsManageViewModel(IList<Claim> claims, IdentityRole role, IPermissionService permissionService)
         {
             List<PermissionItemViewModel> policyItemViewModel = new List<PermissionItemViewModel>();
             foreach (var permission in permissionService.GetPermissions())
             {
-                //Tiene la politica la coleccion de claims
+                //Has the claim collection got the Permission?
                 var exist = claims.Any(x => string.Compare(x.Type, permission.PermissionName, true) == 0);
                 policyItemViewModel.Add(new PermissionItemViewModel { PermissionId = permission.PermissionId, PermissionName = permission.PermissionName, PermissionDescription = permission.PermissionDescription, IsActive = exist, PermissionGroup = permission.PermissionGroup });
             }
