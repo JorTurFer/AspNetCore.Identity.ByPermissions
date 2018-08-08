4583bd71cf73dacbfed365b57c6f904f4d53dbcf
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-08-08 20:58:07 +0200

    Added the Permission attribute

--- src/Identity By Permissions Example/Controllers/HomeController.cs
+++ src/Identity By Permissions Example/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Identity_By_Permissions_Example.Models;
+using AspNetCore.Identity.ByPermissions;
 
 namespace Identity_By_Permissions_Example.Controllers
 {
@@ -14,14 +15,14 @@ namespace Identity_By_Permissions_Example.Controllers
         {
             return View();
         }
-
+        [Permission("About",  "Can see about page")]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
-
+        [Permission("Contact", "Can see contact page")]
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
