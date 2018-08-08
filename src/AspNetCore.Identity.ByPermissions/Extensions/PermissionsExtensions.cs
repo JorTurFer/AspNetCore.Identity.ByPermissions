6212d29ca70b105692c314bbbd8ab52e64a15594
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-08-08 22:14:06 +0200

    Correct the doc of AddPermissions

--- src/AspNetCore.Identity.ByPermissions/Extensions/PermissionsExtensions.cs
+++ src/AspNetCore.Identity.ByPermissions/Extensions/PermissionsExtensions.cs
@@ -7,7 +7,11 @@ namespace AspNetCore.Identity.ByPermissions
 {
     public static class PermissionsExtensions
     {
-        //Add the attributed permissions to the registry
+        /// <summary>
+        /// Add the attributed permissions to the registry
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="service">An instance of the Permission Service</param>
         public static void AddPermissions(this AuthorizationOptions options, IPermissionService service)
         {
             foreach (var policyItem in service.GetPermissions())
