11adeded0d19b18b2034c890fa3f9bba9418e919
Jorge Turrado Ferrero <jturrado@stt-systems.com>
2018-08-08 17:06:28 +0200

    Added the extension

---
+++ src/AspNetCore.Identity.ByPermissions/Extensions/PermissionsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCore.Identity.ByPermissions
+{
+  public static class PermissionsExtensions
+  {
+    public static void AddPermissions(this AuthorizationOptions options, IPermissionService service)
+    {
+      foreach (var policyItem in service.GetPermissions())
+      {
+        options.AddPolicy(policyItem.PermissionName, policy => policy.RequireClaim(policyItem.PermissionName).Build());
+      }
+    }
+  }
+}
