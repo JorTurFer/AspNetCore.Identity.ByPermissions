e61809f37c9cf28884249bbf5440e70f9fc3e7bb
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-08-26 16:01:12 +0200

    Clear unused asemblies

--- src/Identity By Permissions Example/Models/PermissionsViewModels/UserRolesViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Identity_By_Permissions_Example.Models.PermissionsViewModels
-{
-    public class UserRolesViewModel
-    {
-        public string RoleName { get; set; }
-
-        public bool IsActive { get; set; }
-    }
-}
